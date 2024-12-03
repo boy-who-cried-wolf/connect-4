f847a09f5ba1b6f6d4cc99e561478903a16c7eca
MR-Elbarbary <elbarbary7m@gmail.com>
2024-12-04 00:06:51 +0200

    Edit app (add states and Board)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Board from './Board'
 
 function App() {
   const [score, setScore] = useState({p1:0, p2:0})
+  const [currentPlayer, setCurrentPlayer] = useState(1)
+
 
   return (
     <>
-      <h1>Score {score.p1} - {score.p2} </h1>
+      <h1>Score {score.p1} - {score.p2}</h1>
+      <Board
+        currentPlayer = { currentPlayer }
+        switchPlayer = { (current) => setCurrentPlayer(current === 1? 2:1) }
+      />
     </>
   )
 }
