a4962711f75cbe8bc7b668f0381a5306fad89b63
MR-Elbarbary <elbarbary7m@gmail.com>
2024-12-04 13:37:53 +0200

    Add functions to module

--- src/module.js
+++ src/module.js
@@ -1,6 +1,15 @@
+class node {
+    constructor(state, parent, children, value) {
+        this.state = state
+        this.parent = parent
+        this.children = children
+        this.value = value
+    }
+}
+
 function minimax(node, depth, maxmizingPlayer) {
     let value;
-    if (depth === 0 || isTerminal(node)) {
+    if (depth === 0 || isTerminal(node.state)) {
         return heuristic(node)
     }
     if (maxmizingPlayer) {
@@ -19,10 +28,47 @@ function minimax(node, depth, maxmizingPlayer) {
     }
 }
 
-function isTerminal(){}
+function isTerminal(board){
+    board.forEach((col) =>{
+        if (col[0] === 0) {
+            return false
+        }
+    })
+    return true
+}
 
 function heuristic(node) {
 }
 
 const max =(a, b) => a >= b? a:b;
 const min =(a, b) => a <= b? a:b;
+
+
+function minimaxPruning(node, depth, maxmizingPlayer,alpha = -9999999, beta = 9999999) {
+    let value;
+    if (depth === 0 || isTerminal(node)) {
+        return heuristic(node)
+    }
+    if (maxmizingPlayer) {
+        value = -9999999;
+        node.children.forEach(child => {
+            value = max(value, minimax(child, depth-1, false, alpha, beta))
+            alpha = max( alpha, value)
+            if (beta <= alpha){
+                return value
+            }
+        });
+        return value
+    }
+    else{
+        value = 9999999
+        node.children.forEach(child => {
+            value = min(value, minimax(child, depth-1, true))
+            alpha = max( alpha, value)
+            if (beta <= alpha){
+                return value
+            }
+        });
+        return value
+    }
+}
